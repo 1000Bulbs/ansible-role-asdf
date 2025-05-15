40b32ff60dc39ee4084a705b7206afd68641d797
Fernando Alemán <fernandoaleman@mac.com>
2025-05-14 19:17:32 -0500

    Install plugin versions

--- molecule/default/tests/test_asdf_plugins.py
+++ molecule/default/tests/test_asdf_plugins.py
@@ -8,8 +8,14 @@ vars = load_yaml("../vars/all.yml")
 asdf_user = vars["asdf_user"]
 asdf_plugins = vars["asdf_plugins"]
 
-plugins = [plugin["name"] for plugin in asdf_plugins]
 plugin_names = [plugin["name"] for plugin in asdf_plugins]
+
+plugin_versions = [
+    (plugin["name"], version)
+    for plugin in asdf_plugins
+    if "install" in plugin
+    for version in plugin["install"]
+]
 
 
 @pytest.mark.parametrize("plugin", plugin_names)
@@ -28,10 +34,31 @@ def test_plugin_directory_exists(host, plugin):
 def test_asdf_plugin_list(host):
     """Verify each plugin is listed in `asdf plugin list` output"""
     cmd = host.run(f"sudo -u {asdf_user} asdf plugin list")
-    assert cmd.rc == 0, f"`asdf plugin list` failed for {asdf_user}"
+    assert cmd.rc == 0
     installed_plugins = [
         line.strip() for line in cmd.stdout.splitlines() if line.strip()
     ]
-
     for plugin in plugin_names:
         assert plugin in installed_plugins
+
+
+@pytest.mark.parametrize("plugin,version", plugin_versions)
+def test_install_plugin_version_directories(host, plugin, version):
+    """Check if install directory for plugin version exists and is correct"""
+    home = host.user(asdf_user).home
+    path = f"{home}/.asdf/installs/{plugin}/{version}"
+    d = host.file(path)
+
+    assert d.exists
+    assert d.is_directory
+    assert d.user == asdf_user
+    assert d.group == asdf_user
+
+
+@pytest.mark.parametrize("plugin,version", plugin_versions)
+def test_asdf_list_versions(host, plugin, version):
+    """Check that the version is listed in `asdf list <plugin>` output"""
+    cmd = host.run(f"sudo -u {asdf_user} asdf list {plugin}")
+    assert cmd.rc == 0
+    installed = [line.strip() for line in cmd.stdout.splitlines() if line.strip()]
+    assert any(version in line for line in installed)
